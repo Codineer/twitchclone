3421314e8b98746b271302fb1875d0d519f255fb
Codineer <utkarshgamer294@outlook.com>
2024-03-16 21:39:18 +0530

    commit

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Wrapper } from './wrapper'
+const Sidebar = () => {
+    //fetch followed
+    //fetch recommended
+    return (
+
+        <Wrapper>
+            Sidebar!
+        </Wrapper>
+
+    )
+}
+
+export default Sidebar
---
+++ app/(browse)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar } from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
+export default function BrowseLayout(
+    { children }: { children: React.ReactNode }
+) {
+    return (
+        <>
+            <Navbar />
+            <div className="flex h-full pt-20">
+                <Sidebar />
+                {children}
+            </div>
+        </>
+    )
+}
