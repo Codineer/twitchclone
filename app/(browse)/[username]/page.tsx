ddafbf80ee1ac48bf34834cdf38167e8da085631
Codineer <utkarshgamer294@outlook.com>
2024-03-19 21:18:55 +0530

    commit

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -3,6 +3,7 @@ import { getUserByUsername } from '@/lib/user-service'
 import { notFound } from 'next/navigation'
 import { Actions } from './_components/actions'
 import React from 'react'
+import { isBlockedByUser } from '@/lib/block-service'
 
 type UserPageProps = {
     params: {
@@ -22,13 +23,16 @@ const UserPage = async ({
     }
 
     const isFollowing = await isFollowingUser(user.id)
-
-
+    const isBlocked = await isBlockedByUser(user.id)
+    // if (isBlocked) {
+    //     notFound()
+    // }
     return (
         <div className="flex flex-col gap-y-4">
             <p>Username :{params.username}</p>
             <p>user ID: {user.id}</p>
             <p>is Following :{`${isFollowing}`}</p>
+            <p>is blocked by this user : {`${isBlocked}`}</p>
             <Actions userId={user.id} isFollowing={isFollowing} />
         </div>
     )
